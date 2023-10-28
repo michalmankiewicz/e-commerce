2843dabe88ba9a141a199744bb4b60b8e3796bb0
Michal Mankiewicz <michal.mankiewicz@itechart-group.com>
2023-10-28 20:06:41 +0200

    feat: getSingle and getAllUsers implemented

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+const StatusCodes = require("http-status-codes");
+const CustomError = require("../errors");
+
+const getAllUsers = async (req, res) => {
+  const users = await User.find({
+    role: "user",
+  }).select("-password");
+
+  res.status(StatusCodes.OK).json({ users });
+};
+
+const getSingleUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+
+  if (!user) {
+    throw new CustomError.NotFoundError(
+      `No user found with id: ${req.params.id}`
+    );
+  }
+
+  res.status(StatusCodes.OK).json({ user });
+};
+
+const showCurentUser = async (req, res) => {
+  res.send("show current user");
+};
+
+const updateUser = async (req, res) => {
+  res.send("update user");
+};
+
+const updateUserPassword = async (req, res) => {
+  res.send("Update User password");
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  showCurentUser,
+  updateUser,
+  updateUserPassword,
+};
